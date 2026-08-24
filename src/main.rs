bbdcfb69e06b37ec1b37ca449b50b5ba7de47381
benedict <benedictiknkeonye@gmail.com>
2026-08-24 07:45:04 +0200

    feat: longest word in the sentence

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::io;
+use std::{collections::HashMap, io};
 
 fn main() {
     // Give the program a sentence or paragraph and have it report:
@@ -8,7 +8,7 @@ fn main() {
 
     println!("Hello, welcome to the text analyser!");
     println!(
-        "This program analyses yourr sentences to provide charcter count, number of words and the longest word in your sentence"
+        "This program analyses your sentences to provide charcter count, number of words and the longest word in your sentence"
     );
 
     let mut sentence = String::new();
@@ -22,6 +22,7 @@ fn main() {
     let number_of_words = find_number_of_words(&sentence);
     println!("Characters: {}", number_of_characters);
     println!("Words: {}", number_of_words);
+    find_longest_word(&sentence);
 }
 
 // Number of characters
@@ -38,3 +39,23 @@ fn find_number_of_words(sentence: &String) -> usize {
 }
 
 // Longest word
+fn find_longest_word(sentence: &String) {
+    // Collect each word with its count in an object
+    let mut word_count_pair: HashMap<&str, usize> = HashMap::new();
+
+    // .split_whitespace here returns a Vec<_>
+    for word in sentence.split_whitespace() {
+        word_count_pair.insert(word, word.len());
+    }
+
+    // max_by_key allows us access to both the key and the value
+    // using .values().max().unwrap() would have returned just the count of the longest word
+    match word_count_pair.iter().max_by_key(|&(_word, length)| length) {
+        Some((word, _)) => {
+            println!("Longest word: {}", word);
+        }
+        None => {
+            println!("The sentence contained no words.");
+        }
+    }
+}
